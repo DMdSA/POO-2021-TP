756571ad4ad3b6d9e89a05fabf19a405e537ddfe
DMdSA <diogomiguel534@gmail.com>
2023-04-01 17:49:08 +0100

    cleanup

---
+++ POO-2021-TP-main/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Collection;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+
+        MenuController menu = new MenuController();
+        menu.run();
+    }
+}
